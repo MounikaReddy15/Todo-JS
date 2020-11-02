983834302956d361a286bdc6e99a0fe715a68401
unknown <mounikareddygonae15@gmail.com>
2020-11-02 16:55:53 +0530

    total tasks

--- js/index.js
+++ js/index.js
@@ -3,23 +3,27 @@
 const addButton  = document.getElementById('add-btn');
 const todo = document.getElementById('task-bar');
 const listDisplay = document.getElementById('list-container');
+const total = document.getElementById('total');
 const localContent = localStorage.getItem('task');
 var results = JSON.parse(localContent);
 
+
+ // local storage
 let task = localStorage.getItem('task')
   ? JSON.parse(localStorage.getItem('task'))
   : []
 
+
+ // to store todo's
 const storeTasks = (todos) => {
-    if(todos) {
+  if(todos) {
     location.reload();
     task.push(todos);
-    
     localStorage.setItem("task", JSON.stringify(task));
-    
-    
+  }
 }
-}
+
+// to delete task
 const removeTask = (text) => {
   console.log(text);
   const index = results.indexOf(text);
@@ -30,30 +34,30 @@ const removeTask = (text) => {
   window.location.reload();
 }
 
+
+// listing of tasks
 var id=0;
 const liMaker = (text) => {
-   
     const li = document.createElement('li');
     li.innerHTML = `<input class= "radioBtn" onclick = "check(id)" job="complete" type="checkbox" id= '${id}' >  &nbsp;&nbsp;`;
-    
-    li.innerHTML += text;
-   
+    li.innerHTML += `<span id="task-list"> ${text} </span>`
     li.innerHTML += `
-     
     <span> <button id= "delete-btn"onclick = "removeTask('${text}')"> Delete </button> </span>`
     listDisplay.appendChild(li)
     id++;
+    total.innerHTML = id +" tasks";
 }
 
- const displayTasks = () => {
-    
- results.forEach((item) => {
-    
+
+// to display tasks
+const displayTasks = () => {
+  results.forEach((item) => {
   liMaker(item)
   
- });
- }
+});
+}
 
+// check-off todo
 const check = (id) => {
   if(task[id].includes(`<strike>`)) {
     task[id] = task[id].replace("<strike>", "");
@@ -63,9 +67,10 @@ const check = (id) => {
     task[id]= `<strike>  ${task[id]}  </strike>`;
   }
   
-    localStorage.setItem("task", JSON.stringify(task));
+  localStorage.setItem("task", JSON.stringify(task));
   
 }
+
 
 addButton.addEventListener('click', () => storeTasks(todo.value));
 displayTasks();
